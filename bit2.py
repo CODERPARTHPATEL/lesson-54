edb1eaa232081b41d08cf33e42150842ff81a71b
CODERPARTHPATEL <nskp.patel09@gmail.com>
2026-02-06 18:53:17 +0000

    lesson 54

---
+++ bit2.py
@@ -0,0 +1,10 @@
+def numberOfBits(n):
+
+    count = 0
+    while (n):
+        count += 1
+        n >>= 1
+
+    return count
+number = int(input('enter your nuumber'))
+print('total bits:',numberOfBits)
